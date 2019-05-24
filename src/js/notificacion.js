f7ab8dc10ba6110a6e7016d74911702acd62651b
valeskaperezrretamal <jperezr@udd.cl>
2019-05-24 12:12:51 -0400

    se cmabia el nombre de modulo, y se agrega contendiode en el cual el trabajador recibe un mensaje de que tiene visita

---
+++ src/js/notificacion.js
@@ -0,0 +1,24 @@
+/notificacion de visita al iniciar como usuario verificado
+let noty = document.getElementById("time__noty") //variable para mandar a llamar la notificacion
+let mostrar = document.getElementById("mostrar") //muestra la notificacion
+let ocultar = document.getElementById("ocultar")//el usuario puede parar la notificacion
+
+let aparecer = function() { //funcion para activar la notificacion con cosole para ver que ocurra
+   console.log("mostrar")
+   let noty = document.getElementById("time__noty")
+   noty.style.animation = "fastup 10s linear forwards"
+   //  animacion: duracion 10seg;
+   noty.classList.add("time__noty")
+}
+
+let desaparecer = function() { //para la notificacion
+   console.log("ocultar")
+   //console para ver el funcionamiento
+   noty.style.animation = ''
+}
+
+//mostrar notificacion
+
+mostrar.addEventListener("click", aparecer)
+
+ocultar.addEventListener("click", desaparecer)
